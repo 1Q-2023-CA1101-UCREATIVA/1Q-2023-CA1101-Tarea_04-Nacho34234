bbe22819d08b05245a72c8615c5c36db713f72b9
Nacho34234 <124839742+Nacho34234@users.noreply.github.com>
2023-03-05 22:00:10 +0000

    Tarea 4 Ignacio

--- main.py
+++ main.py
@@ -1,30 +1,18 @@
 
 import math 
-
-
+y=int(input("grados Verticales del Dardo en y:"))
+x=int(input("grados Horizontales del Dardo en x:"))
+r=math.sqrt((x)**2+(y)**2)
 def puntaje(x,y):
-  r = math.sqrt((x)**2+(y)**2)
-  puntos_ganados=0
-  if(0 <= r <=1):
-    puntos_ganados=10
-    print("En el blanco ganaste ", puntos_ganados )
-  elif(2 <= r <= 4):
-    puntos_ganados=5
-    print("Cerca ganas", puntos_ganados)
-  elif(5 <= r <= 10):
-    puntos_ganados=1
-    print("lejos ganas solo", puntos_ganados )
-  else:
-    puntos_ganados=0
-    print("cayo fuera", puntos_ganados )
-  return puntos_ganados
-
-
-#x=int(input("grados Horizontales del Dardo x:"))
-#y=int(input("grados Verticales del Dardo y:"))
-x = 0
-y = 0
+  return math.sqrt((x)**2+(y)**2)
 resultado=puntaje(x,y)
-
 print(resultado)
+if(r <=1):
+  print("En el blanco ganaste 10 puntos")
+elif(5 <= r <= 10):
+  print("Lejos ganas 1 puntos")
+elif(1 <= r <= 5):
+  print("Cerca ganas solo 5 punto")
+else:
+  print("cayo fuera 0 puntos")
 
